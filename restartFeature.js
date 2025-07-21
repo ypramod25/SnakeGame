d808d793190d5946a44bcc2b7a10b8053355434e
ypramod25 <py999618@gmail.com>
2025-07-21 11:41:38 +0530

    food start updated

--- restartFeature.js
+++ restartFeature.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const cellSize = 20;
     let score = 0;
     let gameStarted = false;
-    let food = { x: 300, y: 200 };
+    let food = { x: 0, y: 0 };
     let snake = [{ x: 160, y: 200 }, { x: 140, y: 200 }, { x: 120, y: 200 }];
     let dx = cellSize;
     let dy = 0;
